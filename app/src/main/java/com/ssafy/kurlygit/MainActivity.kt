960638247ddc4cfae44be6cd9b1808da1f8541d0
SSAFY <likppi100@naver.com>
2021-12-31 17:36:14 +0900

    21/12/31 - 2-4. 특정 단어(Kotlin)로 검색 시 결과가 잘 나오는지 토스트, 로그로 확인            2-5. 검색한 단어가 잘 전달 되는지 확인            2-6. 검색한 단어로 검색되는지 토스트 및 로그로 확인

--- app/src/main/java/com/ssafy/kurlygit/MainActivity.kt
+++ app/src/main/java/com/ssafy/kurlygit/MainActivity.kt
@@ -18,13 +18,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        val itemList = listOf(1,2,3)
-        val adapter = MainAdapter(itemList)
-
-        binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
+//        val itemList = listOf(1,2,3)
+//        val adapter = MainAdapter(itemList)
+//
+//        binding.recyclerView.adapter = adapter
+//        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        binding.btnSerach.setOnClickListener {
+            val searchWord = binding.edtSearchWord.text.toString()
+            Log.d("Test", "searchWord :  $searchWord")
+            RepositoryService().getRepositories(searchWord, RepositoryCallback())
+        }
+
+
 
     }
 
+    inner class RepositoryCallback : BaseCallback<Repositories> {
+        override fun onSuccess(code: Int, responseData: Repositories) {
+
+            Toast.makeText(this@MainActivity, ""+responseData,Toast.LENGTH_SHORT).show()
+            Log.d("Test", "성공 $responseData")
+
+
+        }
+
+        override fun onError(t: Throwable) {
+            Log.d("Test", "실패 "+t.message ?: "Repository 정보 받아오는 중 통신오류")
+        }
+
+        override fun onFailure(code: Int) {
+            Log.d("Test", "실패 onResponse: Error Code $code")
+        }
+    }
 }
