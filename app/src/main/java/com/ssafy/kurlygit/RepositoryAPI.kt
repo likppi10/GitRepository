4af2283a0f77080f7305453f17631f510bf3ecfa
SSAFY <likppi100@naver.com>
2021-12-31 16:47:49 +0900

    21/12/31 - 2-1. 레트로핏 사용하기 위한 환경설정 :            	RepositoryAPI 생성

---
+++ app/src/main/java/com/ssafy/kurlygit/RepositoryAPI.kt
@@ -0,0 +1,9 @@
+package com.ssafy.kurlygit
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RepositoryAPI {
+    @GET("search/repositories")
+    fun getRepositories(@Query("q") query: String): Call<Repositories>
+}
