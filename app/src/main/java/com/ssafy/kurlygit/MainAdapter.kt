11a6b8dc9aab26e9be2f197fca6e5b0ba21506fe
SSAFY <likppi100@naver.com>
2021-12-31 13:45:27 +0900

    21/12/31 - 1-2. 목록 리스트 구현            	1) 리싸이클러뷰로 구현한다.            	2) 임의의 값으로 리싸이클러 뷰의 아이템이 잘 출력되는지 확인한다.            		(fork, star, title, subtitle)

---
+++ app/src/main/java/com/ssafy/kurlygit/MainAdapter.kt
@@ -0,0 +1,27 @@
+package com.ssafy.kurlygit
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.kurlygit.databinding.ViewholderItemBinding
+
+class MainAdapter(var itemList : List<Int>)
+    : RecyclerView.Adapter<MainViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.viewholder_item, parent, false)
+        return MainViewHolder(ViewholderItemBinding.bind(inflatedView))
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.apply {
+            bind(item)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+}
